84adb699beac822b064f02feb87b31d4ffd0a9ac
leonardovalde <leonardovalde16@gmail.com>
2024-05-31 12:59:19 -0500

    fix iamges

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 module.exports = withNextIntl({
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'shopy-savvy-web.vercel.app'],
   },
 });
